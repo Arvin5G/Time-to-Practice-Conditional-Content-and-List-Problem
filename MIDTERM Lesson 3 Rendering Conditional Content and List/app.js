d11d722cc4ec168b8bc4c34d8a0bfa7a8a376e58
Arvin5G <79516890+Arvin5G@users.noreply.github.com>
2021-11-10 12:15:44 +0800

    added my js code

--- MIDTERM Lesson 3 Rendering Conditional Content and List/app.js
+++ MIDTERM Lesson 3 Rendering Conditional Content and List/app.js
@@ -1,1 +1,22 @@
+const app = Vue.createApp({
+  data() {
+    return { 
+    
+    pokusSaGoal:'',
+    goals: [] ,
+    TPgods:true
+    };
+  },
+  
+  methods: {
+  				addGoal() {
+  				
+  								this.goals.push(this.pokusSaGoal);
+  								this.pokusSaGoal = "";
+  								
+  				}
+  }
+});
+
+app.mount('#assignment');
 
